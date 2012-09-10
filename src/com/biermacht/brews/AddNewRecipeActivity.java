c54c972e37bddf036f4d050715750453186f4155
Casey <davenport.cas@gmail.com>
2012-09-09 23:36:46 -0700

    Icons / styles, Add new recipe activity

---
+++ src/com/biermacht/brews/AddNewRecipeActivity.java
@@ -0,0 +1,45 @@
+package com.biermacht.brews;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class AddNewRecipeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_recipe);
+        
+        // Action bar stuff
+        TextView customActionBar = (TextView) findViewById(R.layout.custom_action_bar);
+        getActionBar().setCustomView(customActionBar);
+        
+        // View holders
+        Spinner beerTypeSpinner;
+        EditText recipeNameEditText;
+        EditText recipeDescriptionEditText;
+        
+        //Arraylist of beer types
+        ArrayList<String> beerTypeArray = Recipe.getBeerTypeList();
+        
+        // Set up beer type spinner
+        beerTypeSpinner = (Spinner) findViewById(R.id.beer_type_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, beerTypeArray);  
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        beerTypeSpinner.setAdapter(adapter);
+        beerTypeSpinner.setSelection(0); 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_add_new_recipe, menu);
+        return true;
+    }
+}
