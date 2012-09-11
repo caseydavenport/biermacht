93abbd08c27fc0bf0eabd59330fa43cf42c9f540
Casey <davenport.cas@gmail.com>
2012-09-10 20:57:30 -0700

    Implemented search, moved fake Recipes to Utils

--- src/com/biermacht/brews/MainActivity.java
+++ src/com/biermacht/brews/MainActivity.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
@@ -17,6 +20,12 @@ public class MainActivity extends ListActivity {
 	// Create our stuff
 	private RecipeArrayAdapter mAdapter;
 	private OnItemClickListener mClickListener;
+	private TextWatcher mTextWatcher;
+	private ArrayList<Recipe> recipeList;
+	
+    //Declare views here
+    private ListView listView; 
+    private EditText searchView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,26 +33,12 @@ public class MainActivity extends ListActivity {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //Make views here
-        ListView listView = getListView();
-
+        // Initialize views
+        listView = getListView();
+        searchView = (EditText) findViewById(R.id.search_bar);
         
-        // Create a bunch of test brews here
-        Recipe brew1 = new Recipe("Arizona Pale Ale");
-        Recipe brew2 = new Recipe("Panther Stout");
-        Recipe brew3 = new Recipe("Chattanooga Cherry Weissbier");
-        Recipe brew4 = new Recipe("Sherman Light");
-        
-        brew1.setBeerType(Recipe.BEERTYPE_IPA);
-        brew2.setBeerType(Recipe.BEERTYPE_STOUT);
-        brew3.setBeerType(Recipe.BEERTYPE_HEFEWEIZEN);
-        
-        // Add the test brews to the list
-        ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
-        recipeList.add(brew1);
-        recipeList.add(brew2);
-        recipeList.add(brew3);
-        recipeList.add(brew4);
+        // Get recipes to display
+        recipeList = Utils.getRecipeList();
         
         // Set up the onClickListener
         mClickListener = new OnItemClickListener() 
@@ -58,9 +53,26 @@ public class MainActivity extends ListActivity {
 			}
         };
         
+        // Set up the textWatcher for search bar
+        mTextWatcher = new TextWatcher() {
+			public void afterTextChanged(Editable s) 
+			{
+			} 
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) 
+            {
+            }
+            
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            	ArrayList<Recipe> fList = getFilteredList(s.toString());
+            	updateRecipeList(fList);
+            }
+        };
+        
+        // Add TextWatcher to search bar
+        searchView.addTextChangedListener(mTextWatcher);
+        
         // Set up my listView with title and ArrayAdapter
-        mAdapter = new RecipeArrayAdapter(this, recipeList);
-        listView.setAdapter(mAdapter);
+        updateRecipeList(recipeList);
         listView.setOnItemClickListener(mClickListener);
         
     }
@@ -82,5 +94,32 @@ public class MainActivity extends ListActivity {
     		break;
     	}
     	return true;
+    }
+    
+    /**
+     * Filters the list of all Recipes by the given string
+     * @param s String to filter by
+     * @return ArrayList of matching recipes
+     */
+    private ArrayList<Recipe> getFilteredList(String s)
+    {
+    	recipeList = Utils.getRecipeList();
+    	ArrayList<Recipe> filteredList = new ArrayList<Recipe>();
+    	
+    	for (Recipe r : recipeList)
+    	{
+    		if(r.getRecipeName().toLowerCase().contains(s.toLowerCase()))
+    		{
+    			filteredList.add(r);
+    		}
+    	}  
+    	return filteredList;
+    }
+    
+    private void updateRecipeList(ArrayList<Recipe> l)
+    {
+        // Set up my listView with title and ArrayAdapter
+        mAdapter = new RecipeArrayAdapter(getApplicationContext(), l);
+        listView.setAdapter(mAdapter);
     }
 }
