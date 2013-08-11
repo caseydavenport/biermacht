0e84f3ce57853c8b97675c0f99884554d9a16be4
Casey D <davenport.cas@gmail.com>
2013-08-10 17:34:29 -0700

    Added Roboto Font and Custom TextView for navbar

---
+++ src/com/biermacht/brews/widget/CustomFontTextView.java
@@ -0,0 +1,31 @@
+package com.biermacht.brews.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.biermacht.brews.R;
+
+public class CustomFontTextView extends TextView {
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.CustomFontTextView);
+        String fontName = styledAttrs.getString(R.styleable.CustomFontTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+    }
+
+}
