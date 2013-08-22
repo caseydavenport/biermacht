d560543a65008b8dd9f0c8d209a4c58770197129
Casey D <davenport.cas@gmail.com>
2013-08-21 20:52:02 -0700

    Updated add misc activity

--- src/com/biermacht/brews/frontend/IngredientActivities/AddMiscActivity.java
+++ src/com/biermacht/brews/frontend/IngredientActivities/AddMiscActivity.java
@@ -18,49 +18,16 @@ import java.util.*;
 
 import com.biermacht.brews.frontend.adapters.SpinnerAdapter;
 
-public class AddMiscActivity extends Activity {
-
-    // Main view - holds all the rows
-    private ViewGroup mainView;
-
-    // Alert builder
-    private AlertBuilder alertBuilder;
-
-    // Important things
-    private View.OnClickListener onClickListener;
-
-    // LayoutInflater
-    private LayoutInflater inflater;
-
-    // Recipe we are editing
-    public Recipe mRecipe;
-
-    // IngredientHandler to get ingredient arrays
-    IngredientHandler ingredientHandler;
+public class AddMiscActivity extends AddEditActivity {
 
     // Holds the currently selected misc, and misc being edited
     public Misc misc;
 
     // Editable rows to display
-    private Spinner miscSpinner;
     private Spinner typeSpinner;
     private Spinner useSpinner;
-    private View nameView;
-    private View amountView;
-    private View timeView;
-
-    // Titles from rows
-    private TextView nameViewTitle;
-    private TextView amountViewTitle;
-    private TextView timeViewTitle;
-
-    // Content from rows
-    public TextView nameViewText;
-    public TextView amountViewText;
-    public TextView timeViewText;
 
     // Spinner array declarations
-    private ArrayList<Ingredient> miscArray;
     private ArrayList<String> typeArray;
     private ArrayList<String> useArray;
 
@@ -71,113 +38,17 @@ public class AddMiscActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_edit);
-
-        // Set icon as back button
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Get the Ingredient Handler
-        ingredientHandler = MainActivity.ingredientHandler;
-
-        // Get the list of ingredients to show
-        miscArray = new ArrayList<Ingredient>();
-        miscArray.addAll(ingredientHandler.getMiscsList());
-
-        // Get the inflater
-        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        // Create alert builder
-        alertBuilder = new AlertBuilder(this);
 
         // Disable delete button for this view
         findViewById(R.id.delete_button).setVisibility(View.GONE);
 
-        // Get recipe and misc from calling activity
-        long id = getIntent().getLongExtra(Constants.KEY_RECIPE_ID, Constants.INVALID_ID);
-
-        // Acquire recipe
-        try
-        {
-            mRecipe = Database.getRecipeWithId(id);
-        }
-        catch (ItemNotFoundException e)
-        {
-            e.printStackTrace();
-            finish();
-        }
-
-        // On click listener
-        onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /************************************************************
-                 * Options for clicking on each of the editable views
-                 ************************************************************/
-
-                AlertDialog alert;
-                if (v.equals(nameView))
-                    alert = alertBuilder.editTextStringAlert(nameViewText, nameViewTitle).create();
-                else if (v.equals(amountView))
-                    alert = alertBuilder.editTextFloatAlert(amountViewText, amountViewTitle).create();
-                else if (v.equals(timeView))
-                    alert = alertBuilder.editTextFloatAlert(timeViewText, timeViewTitle).create();
-                else
-                    return; // In case its none of those views...
-
-                // Force keyboard open and show popup
-                alert.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                alert.show();
-            }
-        };
-
         // Initialize views and such here
-        mainView = (ViewGroup) findViewById(R.id.main_layout);
-        nameView = (View) inflater.inflate(R.layout.row_layout_edit_text, mainView, false);
-        amountView = (View) inflater.inflate(R.layout.row_layout_edit_text, mainView, false);
-        timeView = (View) inflater.inflate(R.layout.row_layout_edit_text, mainView, false);
-        miscSpinner = (Spinner) inflater.inflate(R.layout.row_layout_spinner, mainView, false);
         typeSpinner = (Spinner) inflater.inflate(R.layout.row_layout_spinner, mainView, false);
         useSpinner = (Spinner) inflater.inflate(R.layout.row_layout_spinner, mainView, false);
 
-        // Set the onClickListener for each row
-        nameView.setOnClickListener(onClickListener);
-        amountView.setOnClickListener(onClickListener);
-        timeView.setOnClickListener(onClickListener);
-
-        /************************************************************************
-         ************* Add views to main view  **********************************
-         *************************************************************************/
-        mainView.addView(miscSpinner);
-        mainView.addView(nameView);
+        // Add views to main view
         mainView.addView(useSpinner);
         mainView.addView(typeSpinner);
-        mainView.addView(timeView);
-        mainView.addView(amountView);
-
-        /************************************************************************
-         ************* Get titles, set values   **********************************
-         *************************************************************************/
-        nameViewTitle = (TextView) nameView.findViewById(R.id.title);
-        nameViewTitle.setText("Name");
-
-        amountViewTitle = (TextView) amountView.findViewById(R.id.title);
-        amountViewTitle.setText("Amount (L)");
-
-        timeViewTitle = (TextView) timeView.findViewById(R.id.title);
-        timeViewTitle.setText("Time (mins)");
-
-        /************************************************************************
-         ************* Get content views, set values   **************************
-         *************************************************************************/
-        nameViewText = (TextView) nameView.findViewById(R.id.text);
-        amountViewText = (TextView) amountView.findViewById(R.id.text);
-        timeViewText = (TextView) timeView.findViewById(R.id.text);
-
-        // Set up spinner
-        IngredientSpinnerAdapter adapter = new IngredientSpinnerAdapter(this, miscArray, "Misc Selector");
-        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        miscSpinner.setAdapter(adapter);
-        miscSpinner.setSelection(0);    
 
 		// Set up type spinner
         typeArray = new ArrayList<String>();
@@ -205,30 +76,6 @@ public class AddMiscActivity extends Activity {
 		miscUseAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         useSpinner.setAdapter(miscUseAdapter);
 		useSpinner.setSelection(0);
-
-        // Handle misc selector here
-        miscSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-				public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-					misc = (Misc) miscArray.get(position);
-
-                    nameViewText.setText(misc.getName());
-
-					timeViewText.setText(String.format("%d", mRecipe.getBoilTime()));
-					amountViewText.setText(String.format("%2.2f", misc.getDisplayAmount()));
-					
-					typeSpinner.setSelection(typeArray.indexOf(misc.getMiscType()));
-					useSpinner.setSelection(useArray.indexOf(misc.getUse()));
-					
-					amountViewTitle.setText("Amount (" + misc.getDisplayUnits() + ")");
-					timeViewTitle.setText(misc.getUse() + " time");
-				}
-
-				public void onNothingSelected(AdapterView<?> parentView)
-                {
-				}
-
-			});   
 
 		// Handle type selector here
         typeSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -278,63 +125,91 @@ public class AddMiscActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.activity_add_miscs, menu);
-        return true;
+    public void onMissedClick(View v)
+    {
+
     }
 
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-    	switch (item.getItemId())
-		{
-            case android.R.id.home:
-        		finish();
-        		return true; 
-        }
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void getList()
+    {
+        // Get the list of ingredients to show
+        ingredientList = new ArrayList<Ingredient>();
+        ingredientList.addAll(ingredientHandler.getMiscsList());
     }
 
-	public void onClick(View v) {
-		// if "SUBMIT" button pressed
-		if (v.getId() == R.id.submit_button)
-		{
-            boolean readyToGo = true;
+    @Override
+    public void createSpinner()
+    {
+        // Set up spinner
+        IngredientSpinnerAdapter adapter = new IngredientSpinnerAdapter(this, ingredientList, "Misc Selector");
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerView.setAdapter(adapter);
+    }
 
-            try {
-                String name = nameViewText.getText().toString();
-                int time = Integer.parseInt(timeViewText.getText().toString());
-                int endTime = mRecipe.getBoilTime();
-                int startTime = endTime - time;
-                double amount = Double.parseDouble(amountViewText.getText().toString());
+    @Override
+    public void setInitialSpinnerSelection()
+    {
+        spinnerView.setSelection(0);
+    }
 
-                if (time > mRecipe.getBoilTime())
-                    time = mRecipe.getBoilTime();
+    @Override
+    public void configureSpinnerListener()
+    {
+        spinnerListener = new OnItemSelectedListener() {
 
-                misc.setName(name);
-                misc.setTime(time);
-                misc.setDisplayAmount(amount, misc.getDisplayUnits());
-                misc.setMiscType(type);
-                misc.setUse(use);
-            } catch (Exception e)
-            {
-                Log.d("AddMiscActivity", "Exception on submit: " + e.toString());
-                readyToGo = false;
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                misc = (Misc) ingredientList.get(position);
+
+                nameViewText.setText(misc.getName());
+
+                timeViewText.setText(String.format("%d", mRecipe.getBoilTime()));
+                amountViewText.setText(String.format("%2.2f", misc.getDisplayAmount()));
+
+                typeSpinner.setSelection(typeArray.indexOf(misc.getMiscType()));
+                useSpinner.setSelection(useArray.indexOf(misc.getUse()));
+
+                amountViewTitle.setText("Amount (" + misc.getDisplayUnits() + ")");
+                timeViewTitle.setText(misc.getUse() + " time");
             }
 
-            if (readyToGo)
+            public void onNothingSelected(AdapterView<?> parentView)
             {
-                mRecipe.addIngredient(misc);
-                mRecipe.update();
-                Database.updateRecipe(mRecipe);
-                finish();
             }
-		}
 
-		// if "CANCEL" button pressed
-		if (v.getId() == R.id.cancel_button)
-		{
-			finish();
-		}
-	}
+        };
+    }
+
+    @Override
+    public void onFinished()
+    {
+        mRecipe.addIngredient(misc);
+        mRecipe.update();
+        Database.updateRecipe(mRecipe);
+        finish();
+    }
+
+    @Override
+    public void acquireValues() throws Exception
+    {
+        super.acquireValues();
+
+        misc.setName(name);
+        misc.setTime(time);
+        misc.setDisplayAmount(amount, misc.getDisplayUnits());
+        misc.setMiscType(type);
+        misc.setUse(use);
+    }
+
+    @Override
+    public void onCancelPressed()
+    {
+        finish();
+    }
+
+    @Override
+    public void onDeletePressed()
+    {
+
+    }
 }
